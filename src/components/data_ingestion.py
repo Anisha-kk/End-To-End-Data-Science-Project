386d218977958575d4215bce1bfe8e11e34ccf80
Anisha-kk <akk30272419@gmail.com>
2025-02-04 11:11:04 +0530

    Model Training done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass #decorator
 class DataIngestionConfig:
@@ -49,8 +50,11 @@ if __name__ == "__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
-    #Will execute data_transformation.py functions from here
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
+    #Will execute data_transformation.py functions from here. The third argument is not necessary
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
             
